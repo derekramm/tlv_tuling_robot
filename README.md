729df34f3c8e5d75d2ed026d2eabba40bda55cb2
大蜥蜴 <derek_ramm@icloud.com>
2019-08-29 09:15:02 +0800

    ex01_变量的内置函数的使用

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tlv_tuling_robot
+
 达内直播课特色班项目_图灵机器人API
+
---
+++ client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""client.py"""
+
+import tuling_robot as robot
+
+# 第01步：请在下方 '' 中填写 robot.key，不填写使用默认 APIKEY（有可能失效）
+robot.key = '' or robot.key
+
+if __name__ == '__main__':
+    # 第02步：通过 input() 函数获取输入的内容，并赋值给机器人变量 robot.input_text
+
+    # 第03步：通过 robot.get_values() 函数获取机器人返回的结果，使用 print() 函数打印结果
+
+    pass
