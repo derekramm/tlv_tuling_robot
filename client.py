7cf0a1e59a08ec02478de491c8116087b27ac9d1
derek_ramm <derek_ramm@icloud.com>
2019-08-29 09:34:50 +0800

    函数参数的使用

--- client.py
+++ client.py
@@ -7,31 +7,35 @@ import tuling_robot as robot
 robot.key = '' or robot.key
 
 
+def question(prompt='我'):
+    """
+    接收用户输入的函数
+    :param prompt: 默认提示文本
+    :return: 提示文本 + 用户从终端输入的内容
+    """
+    robot.input_text = input(f'{prompt}：')  # 将用户输入赋值给变量
+    return f'{prompt}：{robot.input_text}'  # 必须添加返回值才可以被写入日志
+
+
+def answer(prompt='机器人'):
+    """
+    机器人回复的函数
+    :param prompt: 默认提示文本
+    :return: 提示文本 + 服务器返回的结果
+    """
+    reply = robot.get_values()
+    return f'{prompt}：{reply}'  # 必须添加返回值才可以被写入日志
+
+
 # 第01步：
-# 定义 quesiont() 函数，通过 input(prompt='我')函数接收输入，并赋值给 robot.input_text 变量
-# question() 函数中需要返回用户的输入
-def question(prompt='我'):  # 练习默认值参数
-    pass
-
-
-# 第02步：
-# 定义 answer() 函数，通过 get_values() 函数接收服务器端返回的结果
-# answer() 函数中需要返回机器人回复的结果
-def answer(prompt='机器人'):  # 练习默认值参数
+# 定义 chat(master_naem, robot_name, *exit_words) 函数，封装用户姓名，机器人姓名和退出关键字列表
+# chat() 函数将之前主函数循环结构的所有代码封装起来
+# 主要练习函数参数的使用
+def chat(master_name, robot_name, *exit_words):
     pass
 
 
 if __name__ == '__main__':
-
-    while True:
-        # 第03步：此处调用 quesiont() 函数
-
-        if robot.input_text in ['88', '886', 'exit', 'quit']:
-            print('机器人：', '拜拜~~')
-            break
-        elif not robot.input_text:
-            continue
-        else:
-            # 第04步：此处调用 answer() 函数
-
-            pass
+    # 第02步：调用定义的 chat() 函数，并给参数赋值
+    # 退出关键字元组示例：*('88', '886', 'exit', 'quit')
+    pass
