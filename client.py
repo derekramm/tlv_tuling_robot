0732cb47ecfa99d15f4c0b65a51dd0268669267b
derek_ramm <derek_ramm@icloud.com>
2019-08-29 09:38:29 +0800

    日志模块 logging 的使用

--- client.py
+++ client.py
@@ -7,6 +7,20 @@ import tuling_robot as robot
 robot.key = '' or robot.key
 
 
+# 第01步：导入日志模块 logging
+# 定义 logging 的配置
+# 日志模块的基本配置
+"""参考：
+import logging
+logging.basicConfig(
+    filename="chat.log",  # 日志文件名称
+    filemode="a",  # 文件操作方式：追加
+    format="%(asctime)s %(name)s:%(levelname)s:%(message)s",  # 日志记录的格式
+    datefmt="%d-%M-%Y %H:%M:%S",  # 日期格式
+    level=logging.DEBUG)  # 日志的级别
+"""
+
+
 def question(prompt='我'):
     """
     接收用户输入的函数
@@ -14,6 +28,9 @@ def question(prompt='我'):
     :return: 提示文本 + 用户从终端输入的内容
     """
     robot.input_text = input(f'{prompt}：')  # 将用户输入赋值给变量
+
+    # 第02步：将 quesiont 写入日志，通过 logging.debug(f'{prompt}：{robot.input_text}')
+
     return f'{prompt}：{robot.input_text}'  # 必须添加返回值才可以被写入日志
 
 
@@ -24,6 +41,9 @@ def answer(prompt='机器人'):
     :return: 提示文本 + 服务器返回的结果
     """
     reply = robot.get_values()
+
+    # 第03步：将 answer 写入日志，通过 logging.debug(f'{prompt}：{reply}')
+
     return f'{prompt}：{reply}'  # 必须添加返回值才可以被写入日志
 
 
